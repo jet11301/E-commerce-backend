e19172234eb6254f347cf575d53af2eeb9ebbded
jet11301 <phuochung113201@gmail.com>
2025-01-09 22:16:50 +0700

    create-databasev2

---
+++ Domain/Entities/CustomerAddresses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CustomerAddresses
+    {
+        public Guid Id { get; set; }
+        [MaxLength(100)]
+        public string? Address_line1 { get; set; }
+        [MaxLength(100)]
+        public string? Address_line2 { get;set; }
+        [MaxLength(6)]
+        public string? Postal_code { get; set; }
+        [MaxLength(50)]
+        public string? Country { get; set; }
+        [MaxLength(50)]
+        public string? City { get; set; }
+        [MaxLength(11)]
+        public string? Phone_number { get; set; }
+        public Customers Customers { get; set; }
+
+    }
+}
--- Domain/Entities/Slideshows.cs
+++ Domain/Entities/Slideshows.cs
@@ -10,15 +10,11 @@ namespace Domain.Entities
     public class Slideshows
     {
         public Guid Id { get; set; }
-
         [MaxLength(255)]
         public string? Image_url { get; set; }
-
         public DateTime Created_at { get; set; } = default;
-
         public DateTime? Updated_at { get; set; }
-
-        public bool? Is_deleted { get; set; }
+        public bool? Is_deleted { get; set; } = false;
 
     }
 }
